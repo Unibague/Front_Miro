8b7d89c936b717f408118329f347c53afc56c889
JonatanAN12 <jonatan25sanchez@gmail.com>
2025-10-30 17:29:17 -0500

    Fix dashboard card titles for consistency and restore templates-with-filters functionality2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-import dotenv from 'dotenv';
-
-dotenv.config();
-const nextConfig = {
-  basePath: process.env.APP_ENV==='development' ? '/dev' : '',
-};
+const nextConfig = {};
 
 export default nextConfig;
